d9fd97db103bc98ef035546b8c42d14458081712
Pacasian <pacasianisland@gmail.com>
2020-07-23 13:23:23 +0530

    xcvcx

---
+++ app/src/main/java/com/example/spellingbook/FloatingBar.java
@@ -0,0 +1,54 @@
+package com.example.spellingbook;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class FloatingBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_bar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(FloatingBar.this);
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(FloatingBar.this);
+                View view2 = layoutInflaterAndroid.inflate(R.layout.alert_box, null);
+                builder.setView(view2);
+                builder.setCancelable(false);
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                ImageButton imageButton= view2.findViewById(R.id.imageButton2);
+                ImageButton imageButton1= view2.findViewById(R.id.imageButton);
+               imageButton.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       Toast.makeText(FloatingBar.this, "Working ...", Toast.LENGTH_SHORT).show();
+                   }
+               });
+                imageButton1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(FloatingBar.this, "Working 1...", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/spellingbook/eachAlphabet.java
+++ app/src/main/java/com/example/spellingbook/eachAlphabet.java
@@ -3,18 +3,22 @@ package com.example.spellingbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
 import java.util.Objects;
 
 import static com.example.spellingbook.alphabets2screen.eachWord;
 import static com.example.spellingbook.alphabets2screen.wordImage;
 
 public class eachAlphabet extends AppCompatActivity {
-TextView txt;
-ImageView imgView;
+TextView txt,txtPlay;
+ImageView imgView,imageViewPlay;
+    TextToSpeech t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +30,25 @@ ImageView imgView;
         txt=findViewById(R.id.txt_alpha);
         imgView=findViewById(R.id.imageView);
         //set the image and text from the previous activity 
-        String alpha =eachWord[Pos].toUpperCase();
+        final String alpha =eachWord[Pos].toUpperCase();
         txt.setText(alpha);
         imgView.setImageResource(wordImage[Pos]);
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+        txtPlay=findViewById(R.id.txtPlay);
+        txtPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtPlay.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_pause_24, 0, 0, 0);
+                t1.speak(alpha, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+        txtPlay.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_play_arrow_24, 0, 0, 0);
     }
 }
